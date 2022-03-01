c99367d5b9946972018cae9b54e0d2350e0c4305
JabedH <87475551+JabedH@users.noreply.github.com>
2022-03-01 09:45:25 +0600

    add

--- js/node.js
+++ js/node.js
@@ -57,7 +57,7 @@ const getValue = ()=>{
         <h5 class="card-title text-center">brand: ${searchMobile.brand}</h5>
       </div>
       <div class="text-center">
-      <a href="#" class="btn btn-primary">More details</a>
+      <a href="#" onclick="getMobileId('${searchMobile.slug}')" class=" text-center btn btn-primary ">More Details</a>
       </div>
     </div>
   </div>`
@@ -66,11 +66,39 @@ const getValue = ()=>{
     } )
   }
   
-  if(onclick="getValue()"){
-    document.getElementById('hideLatestPhone').style.display = 'none'
-    document.getElementById('hideCarousel').style.display = 'none'
-    
-  }
-  
-
+  // if(onclick="getValue()"){
+  //   document.getElementById('hideLatestPhone').style.display = 'none'
+  //   document.getElementById('hideCarousel').style.display = 'none'
+  // }
 }
+// addId
+const getMobileId = mobilesId =>{
+  
+  console.log(mobilesId)
+  fetch(`https://openapi.programming-hero.com/api/phone/${mobilesId}`)
+  .then(res => res.json())
+  .then(data => showMobileId(data.data))
+}
+const showMobileId = idDetails => {
+  console.log(idDetails)
+  const addMobileId = document.getElementById('addMobileId')
+  const div = document.createElement('div')
+  div.innerHTML =
+  `
+  <div class="card mb-3" style="max-width: 540px;">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="${idDetails.image}" class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">${idDetails.name}</h5>
+          <p class="card-text"><small class="text-muted"></small></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  `
+  addMobileId.appendChild(div)
+}
+
