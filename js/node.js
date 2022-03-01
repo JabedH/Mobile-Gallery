fed0905634acd96836e50f61eb6c82ad816af6d6
JabedH <87475551+JabedH@users.noreply.github.com>
2022-03-01 11:05:06 +0600

    add

--- js/node.js
+++ js/node.js
@@ -29,6 +29,7 @@ const getValue = ()=>{
     const error1 = document.getElementById('error1')
   const searchField = document.getElementById('searchValue')
   const searchValue = searchField.value
+  searchField.value =""
   if(!isNaN(searchValue) || searchValue == ''){
     error.innerText = 'Please search by name'
   }
@@ -37,9 +38,10 @@ const getValue = ()=>{
     fetch(`https://openapi.programming-hero.com/api/phones?search=${searchValue}`)
     .then( res => res.json())
     .then(data => searchItems(data.data.slice(0, 20)))
-    // if(searchValue == false){
-    //   error1.innerText = 'Not Found'
-    // }
+    error.innerText ='';
+    if(searchValue == false){
+      error1.innerText = 'Not Found'
+    }
   }
   const searchItems = searchMobiles =>{
     searchMobiles.forEach(searchMobile =>{
@@ -62,29 +64,53 @@ const getValue = ()=>{
     </div>
   </div>`
   addToCart.appendChild(div)
-      
+
+// Display none
+  if(onclick="getValue()"){
+    document.getElementById('hideLatestPhone').style.display = 'none'
+    // document.getElementById('hideCarousel').style.display = 'none'
+  }
     } )
   }
   
-  // if(onclick="getValue()"){
-  //   document.getElementById('hideLatestPhone').style.display = 'none'
-  //   document.getElementById('hideCarousel').style.display = 'none'
-  // }
 }
 // addId
 const getMobileId = mobilesId =>{
-  
   console.log(mobilesId)
   fetch(`https://openapi.programming-hero.com/api/phone/${mobilesId}`)
   .then(res => res.json())
   .then(data => showMobileId(data.data))
 }
 const showMobileId = idDetails => {
-  console.log(idDetails)
+  console.log(idDetails.releaseDate)
   const addMobileId = document.getElementById('addMobileId')
   addMobileId.innerHTML = '';
   const div = document.createElement('div')
-  div.innerHTML =
+  if(idDetails.releaseDate== ""){
+    const newReleaseDate = 'not found'
+    div.innerHTML =
+    `
+    <div class="card mb-3 border border-info" style="max-width: 700px;">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="${idDetails.image}" class="img-fluid rounded-start p-1" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">${idDetails.name}</h5>
+            <h5 class="card-title">MainFeatures: ${ idDetails.mainFeatures.displaySize}</h5>
+            <h5 class="card-title">Storage: ${ idDetails.mainFeatures.storage}</h5>
+            <h5 class="card-title">Storage: ReleaseDate: ${newReleaseDate}</h5>
+            <h5 class="card-title"><small class="text-muted">sensors: ${idDetails.mainFeatures.sensors}</small></h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    `
+    addMobileId.appendChild(div)
+  }
+  else{
+    div.innerHTML =
   `
   <div class="card mb-3 border border-info" style="max-width: 700px;">
     <div class="row g-0">
@@ -96,6 +122,7 @@ const showMobileId = idDetails => {
           <h5 class="card-title">${idDetails.name}</h5>
           <h5 class="card-title">MainFeatures: ${ idDetails.mainFeatures.displaySize}</h5>
           <h5 class="card-title">Storage: ${ idDetails.mainFeatures.storage}</h5>
+          <h5 class="card-title">releaseDate: ${ idDetails.releaseDate}</h5>
           <h5 class="card-title"><small class="text-muted">sensors: ${idDetails.mainFeatures.sensors}</small></h5>
         </div>
       </div>
@@ -103,5 +130,7 @@ const showMobileId = idDetails => {
   </div>
   `
   addMobileId.appendChild(div)
+  }
 }
+
 
